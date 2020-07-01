07fb253741e26a4dc57ae43073dffbf84c590674
Michael <michaelvini62@gmail.com>
2020-07-01 16:51:36 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+import "../src/style.css";
+import Main from "../src/pages/main"
+
+const App = () => (
+     <div className="App">
+       <Header />
+       <Main />
+     </div>
+);
+
+export default App;
